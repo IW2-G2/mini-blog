dd66fec0c96dbbbdc94a7fce38a63af840efb07d
campsx <cam.laurent@outlook.com>
2017-01-08 20:04:35 +0100

    Ajout de la base du projet

---
+++ views/acticle.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+	require dirname(__FILE__) . '/manager/manager.class.php';
+	$manager = new Manager();
+ ?>
+<html>
+<head>
+	<title>blog</title>
+</head>
+<body>
+
+</body>
+</html>
+
