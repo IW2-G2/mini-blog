514d94c53a8c16a373b6d4af01762d0737f253d5
Kuben <jordan.venant@gmail.com>
2017-01-18 19:14:38 +0100

    ajout du fichier constants.php
    Fichier constants.php servant à factoriser un peu le fichier index.php et à stocker toutes nos constantes

--- config/constants.php
+++ config/constants.php
@@ -1,3 +1,6 @@
 <?php
 define("DS", DIRECTORY_SEPARATOR);
 define("SRC", dirname(__FILE__).DS);
+define("APP_BASE_PATH", "/mini-blog/");
+define("APP_BASE_PATH_PATTERN", "\/mini-blog\/");
+define("VIEW_FOLDER_PATH", "App".DS."View".DS);
